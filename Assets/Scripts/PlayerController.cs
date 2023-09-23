9e17d3414553e2832376f429ab2521338b1453ab
RachelC1995 <rcoffey05@qub.ac.uk>
2023-09-23 14:24:55 +0100

    Camera switcher added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,11 @@ public class PlayerController : MonoBehaviour
     private float horizontalInput;
     private float forwardInput;
 
+    // Switch camera view
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+
         // Start is called before the first frame update
     void Start()
     {
@@ -27,5 +32,10 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         // We turn the vehicle
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        if(Input.GetKeyDown(switchKey))
+
+        mainCamera.enabled = !mainCamera.enabled;
+        hoodCamera.enabled = !hoodCamera.enabled;
     }
 }
